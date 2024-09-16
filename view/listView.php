9abb9f5791a9682647f09fad4d3b722a19e1e7eb
Gus5900000 <augustinverissimo.pro@gmail.com>
2024-09-16 16:00:56 +0200

    Finish

--- view/listView.php
+++ view/listView.php
@@ -19,24 +19,22 @@
 
     <!-- Créer un main avec un tableau qui présente les différents évènements de la base de données-->
     <main>
-        <?php
+    <?php
         foreach($eventList as $event)  {
             ?>
-        <div class="eventImg">
-            <?= $event['image'] ?> 
+        <div class="card-container">
+            <div class="card" >
+                <img src="<?= $event['image'] ?>" class="card-img-top" alt="Image">
+                    <div class="card-body">
+                    <h5 class="card-title"><?= $event['lieu'] ?> </h5>
+                    <p class="card-text"><?= $event['description'] ?> </p>
+                    <p class="card-text"><?= $event['difficulte'] ?> </p>
+                </div>
+            </div>  
         </div>
-        <div class="eventLieu">
-            <?= $event['lieu'] ?> 
-        </div>
-        <div class="eventDescription">
-            <?= $event['description'] ?> 
-        </div>
-        <div class="eventDifficulte">
-            <?= $event['difficulte'] ?> 
-        </div>
-        <?php
-        }
-        ?>
+    <?php
+    }
+    ?>
     </main>
 </body>
 
